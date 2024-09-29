f08c1f0221e502eceffeb8ba3a8e56b8d5d9f44d
Angella-Mulikatete <mulikateteangella@gmail.com>
2024-09-29 23:50:13 +0300

    updated hardhat config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,32 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
+   networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    scrollSepolia: {
+      url: process.env.SCROLL_RPC_URL,
+      accounts: [process.env.ACCOUNT_PRIVATE_KEY!],
+    },
+  },
+  
+  etherscan: {
+    apiKey:{
+      // arbitrumSepolia: "11XBITUSRTETURM7D2JTXPE49ZG84GPMX7",
+    }
+  },
 };
+
+
+
+
+
+
+
 
 export default config;
